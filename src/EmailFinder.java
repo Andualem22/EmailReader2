635c3e06858d450b117dbbb61cab179a8c411109
Nate <dfenlb@gmail.com>
2023-01-10 00:22:45 -0600

    Adding conditional Statement to verify if an email address was found

--- src/EmailFinder.java
+++ src/EmailFinder.java
@@ -20,6 +20,7 @@ public class EmailFinder {
 	    System.out.print("Enter Last Name : ");
 	      // Read user input
 	    String lastName = inputLastName.nextLine();
+	    boolean emailFound = false;
 	    //System.out.println("Username is: " + userName);  // Output user input
 	    
 		/*
@@ -45,7 +46,7 @@ public class EmailFinder {
 				 
 			}
 			
-			for (int i=0; i<=lines1.size(); i++)
+			for (int i=0; i<=lines1.size() - 1; i++)
 			{
 				String email1 = lines1.get(i).toString();
 				String emailLastName = email1.split("@")[0];
@@ -57,6 +58,7 @@ public class EmailFinder {
 					// if the names match, print the email address and break out of the loop
 					System.out.println(lines1.get(i).toString());
 					System.out.println("Customer "+firstName+" "+lastName+ " email address is "+ lines1.get(i).toString());
+					emailFound = true;
 					break;
 				}
 				//This is for customers/clients that have their names and email listed together. 
@@ -68,11 +70,16 @@ public class EmailFinder {
 					if(firtAndLastName.equalsIgnoreCase(customerFullName.replace(" ", ""))){
 						System.out.println(lines1.get(i).toString());
 						System.out.println("Customer "+firstName+" "+lastName+ " email address is "+ customerNameAndEmail[1]);
+						emailFound = true;
 						break;
 					}
 				}
 				
 			}
+			if(emailFound == false) { 
+				System.out.println("Customer "+firstName+" "+lastName+" email not found in the provided data");
+			}
+			
 		} catch (IOException e) {
 			// handle exception
 			e.printStackTrace();
